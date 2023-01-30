830a3762dedfbfd58ee6ae32baa3754d9e070d53
Eduardo Vidal <eduardo.vidal@snlv.com.br>
2023-01-30 16:11:22 -0300

    Remove modulo de flow do terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 module "s3-static-website" {
-  source = "git@github.com:snlv-com-br/terraform_modules.git//aws/s3_static_hosting"
+  source = "cn-terraform/s3-static-website/aws"
 
   providers = {
     aws.main         = aws
@@ -24,8 +24,6 @@ module "s3-static-website" {
   website_error_document = "index.html"
   website_index_document = "index.html"
 
-  user_name = var.AWS_USER_NAME
-
   # CORS
   # website_cors_allowed_headers = ["*"]
   # website_cors_allowed_methods = ["GET", "PUT", "POST", "DELETE", "HEAD"]
@@ -35,13 +33,28 @@ module "s3-static-website" {
 
   comment_for_cloudfront_website = "CloudFront distribution para o portal hoppscotch. Gestor de colecoes de APIs"
 
-  # abla.one
+  cloudfront_custom_error_responses = [
+    {
+      error_caching_min_ttl = 300
+      error_code            = 403
+      response_code         = 200
+      response_page_path    = "/index.html"
+    },
+    {
+      error_caching_min_ttl = 300
+      error_code            = 404
+      response_code         = 200
+      response_page_path    = "/index.html"
+    }
+  ]
+
+  # abla.com.br
   create_route53_hosted_zone = false
-  route53_hosted_zone_id     = "Z09148111EOX0T0NFNVW2" 
+  route53_hosted_zone_id     = "Z02901972GYE28YO8W4MZ"
 
   # *.abla.one
-  create_acm_certificate = false
-  acm_certificate_arn_to_use = "arn:aws:acm:us-east-1:865320629804:certificate/e8506a30-52ea-4591-b66a-a0e667e0f6bc"
+  # create_acm_certificate = false
+  # acm_certificate_arn_to_use = "arn:aws:acm:us-east-1:865320629804:certificate/e8506a30-52ea-4591-b66a-a0e667e0f6bc"
 
   tags = {
     "projeto" = "hoppscotch"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,8 +2,3 @@ variable "DOMAIN_URL" {
   type        = string
   description = "Website domain name"
 }
-
-variable "AWS_USER_NAME" {
-  type        = string
-  description = "User responsible for uploading build files"
-}
